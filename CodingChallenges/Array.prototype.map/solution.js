7a69d3af7e11c8f94ee218afc3db2e327b634f71
abhishek.rdx <codeforstartups@gmail.com>
2025-11-26 04:33:12 +0530

    Array.prototype.map

---
+++ CodingChallenges/Array.prototype.map/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @template T, U
+ * @param { (value: T, index: number, array: Array<T>) => U } callbackFn
+ * @param {any} [thisArg]
+ * @return {Array<U>}
+ */
+Array.prototype.myMap = function (callbackFn, thisArg) {
+  const len = this.length;
+  const array = new Array(len);
+
+  for (let k = 0; k < len; k++) {
+    // Ignore index if value is not defined for index (e.g. in sparse arrays).
+    if (Object.hasOwn(this, k)) {
+      array[k] = callbackFn.call(thisArg, this[k], k, this);
+    }
+  }
+
+  return array;
+};
